5b220d6849623fe2c146cda2ddf3ad51c92c8665
Matt Hayward <mjhayward@gmail.com>
2016-11-27 02:32:30 -0800

    Upgrading to cedar-14

--- viz/urls.py
+++ viz/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns( '',
                         url( r'^script_data/(?P<script_id>[\d]+)/(?P<data_structure>[\w]+)/$', views.script_data, name='script_data' ),
                         url( r'^vizualize/script/(?P<script_id>[\d]+)/', views.vizualize, name='vizualize' ),
                         url( r'^load_script', views.load_script, name='load_script' ),
-                        url( r'^upload_script/', views.upload_script, name='upload_script' ),
+                        #url( r'^upload_script/', views.upload_script, name='upload_script' ),
                         url( r'', views.default, name='default' ),
 #                        url( r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
                         )
